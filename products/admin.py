ffed6cbd2a5196fe40bd9f9ffd792cb643771578
ju4ndrc <jdramirez90@ucatolica.edu.co>
2025-12-18 12:15:19 -0500

    [+]Auth added for admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from products.models import Product
+
+
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product 
+    list_display = ['name','price','stock','photo']
+    search_fields = ['name']
+admin.site.register(Product, ProductAdmin)
