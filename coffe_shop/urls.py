068f22f028d3689a07ae6844e1b8fab97dfc2b24
ju4ndrc <jdramirez90@ucatolica.edu.co>
2025-12-19 11:04:51 -0500

    [+]Home page added

--- coffe_shop/urls.py
+++ coffe_shop/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include("home.urls")),
     path('admin/', admin.site.urls),
     path('products/',include("products.urls")),
     path('users/',include("users.urls")),
--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from django.contrib.auth.views import LoginView ,LogoutView
 
 urlpatterns = [
     path("login",LoginView.as_view(template_name="users/login.html"),name="login"),
-    path("logout",LogoutView.as_view(template_name="users/login.html"),name="logout"),
+    path("logout",LogoutView.as_view(template_name="users/logout.html"),name="logout"),
 ]
