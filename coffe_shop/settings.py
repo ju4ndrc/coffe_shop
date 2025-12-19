20b1fe414b056547421c0a47bf692d312e1a3458
ju4ndrc <jdramirez90@ucatolica.edu.co>
2025-12-18 22:31:40 -0500

    [+]using alpine ui,[+]diferent navbars,[!]errors fixed

--- coffe_shop/settings.py
+++ coffe_shop/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 
@@ -132,3 +132,7 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 
 LOGIN_REDIRECT_URL = "products_list"
 LOGOUT_REDIRECT_URL = "products_list"
+
+STATICFILES_DIRS = [
+    BASE_DIR/"static"
+]
